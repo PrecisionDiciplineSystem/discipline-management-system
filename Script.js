d139885d55b5e31a0c23aa961baa76d220e275ee
PrecisionDisciplineSystem <abaugher@precisionvs.com>
2026-07-23 16:48:12 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,22 @@
+const msalConfig = {
+    auth: {
+        clientId: "8942b408-2646-4bbd-bb4f-7a82226351c2",
+        authority: "https://login.microsoftonline.com/common",
+        redirectUri: window.location.origin
+    }
+};
 
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+
+async function signIn() {
+
+    const loginResponse = await msalInstance.loginPopup({
+        scopes: [
+            "User.Read"
+        ]
+    });
+
+    console.log(loginResponse.account);
+
+}
